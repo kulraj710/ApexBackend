a6a2054eadc643769346be966dfecead7d284098
kulraj710 <kulraj710@gmail.com>
2023-01-27 01:44:32 +0530

    empty

--- apex/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import PatProfile, OpdRecord
-
-# Register your models here.
-admin.site.register(PatProfile)
-admin.site.register(OpdRecord)
--- apex/helpers.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def validate_is_date(date_string):
-    pattern = re.compile(r"^\d{4}-\d{2}-\d{2}$", re.IGNORECASE)
-    if pattern.match(date_string):
-        return True
-    else:
-        return False
